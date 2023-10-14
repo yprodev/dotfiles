1a2b20b07486d4e42c9b3dced9b8155d97ea6ce9
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2023-10-14 17:53:21 +0300

    Alter the script to install nvim of the specific version

--- install-nvim.sh
+++ install-nvim.sh
@@ -6,6 +6,20 @@ command -v nvim >/dev/null
 
 if [[ $? -ne 0 ]]; then
     echo "Nvim is not installed"
+
+    sudo apt remove neovim -y
+    curl -LO https://github.com/neovim/neovim/releases/download/v0.9.1/nvim.appimage
+    chmod u+x nvim.appimage
+    sudo mv nvim.appimage /usr/bin/nvim
+    # Check NeoVim version installed
+    nvim --version
+
+    # TODO: how to add alias with if
+
+    echo 'alias vim=nvim' >> ~/.zshrc
+    source ~/.zshrc
+    # Check vim updated Alias
+    vim --version
 else
     nvim_version=$(nvim --version | head -1 | grep -o '[0-9]\.[0-9]')
 
